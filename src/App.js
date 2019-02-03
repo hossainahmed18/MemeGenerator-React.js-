c8dcf1382dedde890710b4ab3104bb79a2d1ff71
hossainahmed18 <junayedhossain77@gmail.com>
2019-02-03 21:35:28 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react"
+import MemGenerator from "./mem/MemGenerator"
+import Header from "./mem/Header"
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render(){
+        return (
+            <div>
+                 <Header />
+                 <MemGenerator/>
+                 <h1 className="fun">This is App</h1>
+             </div>
+        )
+    }
 }
 
-export default App;
+export default App
